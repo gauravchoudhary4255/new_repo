16ad9a3897433b1d36ad87ac6144a3ea2dcb5cd4
gauravchoudhary4255 <gauravchoudhary4255@gmail.com>
2023-11-14 23:27:15 +0530

    new changes done

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-let sum = 0 ;
-function hello(){
-    sum+=1;
-    if(sum>5){
-        console.log("hello the condtion is satisfied")
-        return "cndtion satisfied"
-    }
-    console.log(`printing hello ${sum}`)
-    hello()
-}
-hello()
+// 1. basic recursion uderstanding
+// let sum = 0 ;
+// function hello(){
+//     sum+=1;
+//     if(sum>5){
+//         console.log("hello the condtion is satisfied")
+//         return "cndtion satisfied"
+//     }
+//     console.log(`printing hello ${sum}`)
+//     hello()
+// }
+// hello()
+
+//2.
